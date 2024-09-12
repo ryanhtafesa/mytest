ad125503739207324f266ae1eb0daab7ab821b63
Ryan Higginson <ryan.higginson@student.tafesa.edu.au>
2024-09-13 08:34:55 +0930

    Round 1 Price Change

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -14,9 +14,9 @@ namespace Ticketing
         private decimal amountDue;
         private decimal mPrice;
 
-        const decimal mdecBalcony = 35.5m;
-        const decimal mdecGeneral = 28.75m;
-        const decimal mdecBox = 62.0m;
+        const decimal mdecBalcony = 30.5m;
+        const decimal mdecGeneral = 20.75m;
+        const decimal mdecBox = 60.0m;
         const decimal mdecDiscount = 5.0m; 
 
         private int Section
